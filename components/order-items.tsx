e02b3b38876182be0ae89caaa03959a2fc402f81
Nerwin Alamas <nerwinalamas@gmail.com>
2025-09-13 10:21:03 +0800

    refactor: update and remove item

--- components/order-items.tsx
+++ components/order-items.tsx
@@ -1,14 +1,37 @@
 import ReusableImage from "@/components/reusable-image";
 import { Button } from "@/components/ui/button";
 import { Minus, Plus } from "lucide-react";
+import { useOrderStore } from "@/store/order";
 
 interface OrderItemsProps {
+  id: number;
   imageUrl: string;
   name: string;
   price: number;
+  quantity: number;
 }
 
-const OrderItems = ({ imageUrl, name, price }: OrderItemsProps) => {
+const OrderItems = ({
+  id,
+  imageUrl,
+  name,
+  price,
+  quantity,
+}: OrderItemsProps) => {
+  const { updateQuantity, removeItem } = useOrderStore();
+
+  const handleIncrement = () => {
+    updateQuantity(id, quantity + 1);
+  };
+
+  const handleDecrement = () => {
+    if (quantity === 1) {
+      removeItem(id);
+    } else {
+      updateQuantity(id, quantity - 1);
+    }
+  };
+
   return (
     <div className="flex justify-between items-center text-sm">
       <div className="flex items-center gap-2">
@@ -21,15 +44,22 @@ const OrderItems = ({ imageUrl, name, price }: OrderItemsProps) => {
         />
         <div className="flex flex-col gap-2">
           <span className="text-sm">{name}</span>
-          <span className="font-semibold text-base">${price}</span>
+          <span className="font-semibold text-base">${price.toFixed(2)}</span>
         </div>
       </div>
       <div className="flex items-center gap-3">
-        <Button size="icon" className="rounded-full">
+        <Button
+          size="icon"
+          className="rounded-full"
+          variant="outline"
+          onClick={handleDecrement}
+        >
           <Minus className="w-4 h-4" />
         </Button>
-        <span className="font-semibold">1</span>
-        <Button size="icon" className="rounded-full">
+        <span className="font-semibold min-w-[20px] text-center">
+          {quantity}
+        </span>
+        <Button size="icon" className="rounded-full" onClick={handleIncrement}>
           <Plus className="w-4 h-4" />
         </Button>
       </div>
