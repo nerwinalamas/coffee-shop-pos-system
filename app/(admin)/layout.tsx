401d63cd0e659b7c4cb06fcb402f6e9bd7688ce4
Nerwin Alamas <nerwinalamas@gmail.com>
2025-09-21 12:17:35 +0800

    refactor: add sidebar inset and update layout

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,14 +1,25 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
 import { AppSidebar } from "@/components/app-sidebar";
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+        </header>
+        <main className="flex-1 p-4">{children}</main>
+      </SidebarInset>
     </SidebarProvider>
   );
 };
