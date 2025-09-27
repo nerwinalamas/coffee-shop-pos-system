b1f47183e11c7b17fc0823ae91cc8f237262b3a6
Nerwin Alamas <nerwinalamas@gmail.com>
2025-09-27 14:48:16 +0800

    feat: page header

---
+++ components/page-header.tsx
@@ -0,0 +1,15 @@
+interface PageHeaderProps {
+  title: string;
+  description: string;
+}
+
+const PageHeader = ({ title, description }: PageHeaderProps) => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold tracking-tight">{title}</h1>
+      <p className="text-muted-foreground">{description}</p>
+    </div>
+  );
+};
+
+export default PageHeader;
