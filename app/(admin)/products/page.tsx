29de9deaf55da126ae38409436f4cb99ff382dc9
Nerwin Alamas <nerwinalamas@gmail.com>
2025-09-27 14:48:47 +0800

    refactor: add title and description

--- app/(admin)/products/page.tsx
+++ app/(admin)/products/page.tsx
@@ -1,8 +1,13 @@
+import PageHeader from "@/components/page-header";
 import ProductTable from "./_components/product-table";
 
 const ProductsPage = () => {
   return (
-    <div>
+    <div className="space-y-4">
+      <PageHeader
+        title="Products"
+        description="Manage your product inventory and details"
+      />
       <ProductTable />
     </div>
   );
