caff85953e6ef8026dc54162714e19d26139e688
Nerwin Alamas <nerwinalamas@gmail.com>
2025-09-28 11:34:20 +0800

    feat: create helper function for badge

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getCategoryVariant = (category: string) => {
+  switch (category) {
+    case "Coffee":
+      return "bg-amber-100 text-amber-800 hover:bg-amber-100";
+    case "Food":
+      return "bg-green-100 text-green-800 hover:bg-green-100";
+    case "Dessert":
+      return "bg-pink-100 text-pink-800 hover:bg-pink-100";
+    default:
+      return "bg-gray-100 text-gray-800 hover:bg-gray-100";
+  }
+};
