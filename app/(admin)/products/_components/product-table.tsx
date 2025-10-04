75b1837d3cecfdd5e8428a2b5a5a6b72b9635b40
Nerwin Alamas <nerwinalamas@gmail.com>
2025-10-04 11:55:18 +0800

    refactor: update product table to use reusable data table component

--- app/(admin)/products/_components/product-table.tsx
+++ app/(admin)/products/_components/product-table.tsx
@@ -1,51 +1,16 @@
 "use client";
 
-import { useState } from "react";
+import { ColumnDef } from "@tanstack/react-table";
 import Image from "next/image";
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
 import { PRODUCTS } from "@/app/data";
 import { Product } from "@/types/product.types";
 import { getCategoryVariant } from "@/lib/utils";
-import SearchInput from "@/components/search-input";
+import { DataTable } from "@/components/data-table";
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
 import { Badge } from "@/components/ui/badge";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import {
-  ChevronLeft,
-  ChevronRight,
-  ChevronsLeft,
-  ChevronsRight,
-  Edit2,
-  Trash2,
-  Plus,
-} from "lucide-react";
+import { Edit2, Trash2, Plus } from "lucide-react";
 
 const ProductTable = () => {
-  const [globalFilter, setGlobalFilter] = useState("");
-
   const columns: ColumnDef<Product>[] = [
     {
       accessorKey: "image",
@@ -87,7 +52,6 @@ const ProductTable = () => {
       size: 150,
       cell: ({ row }) => {
         const category = row.original.category;
-
         return (
           <Badge className={`rounded-full ${getCategoryVariant(category)}`}>
             {category}
@@ -105,7 +69,6 @@ const ProductTable = () => {
             <Button variant="outline" size="icon" aria-label="Edit">
               <Edit2 className="w-4 h-4" />
             </Button>
-
             <Button variant="destructive" size="icon" aria-label="Remove">
               <Trash2 className="w-4 h-4" />
             </Button>
@@ -116,171 +79,18 @@ const ProductTable = () => {
     },
   ];
 
-  const table = useReactTable({
-    data: PRODUCTS,
-    columns,
-    columnResizeMode: "onChange",
-    enableColumnResizing: false,
-    state: {
-      globalFilter,
-    },
-    onGlobalFilterChange: setGlobalFilter,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    globalFilterFn: (row, columnId, filterValue) => {
-      const value = row.getValue(columnId);
-      return String(value).toLowerCase().includes(filterValue.toLowerCase());
-    },
-  });
-
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-        <SearchInput
-          searchQuery={globalFilter}
-          setSearchQuery={setGlobalFilter}
-        />
-
+    <DataTable
+      columns={columns}
+      data={PRODUCTS}
+      headerActions={
         <Button className="gap-2">
           <Plus className="w-4 h-4" />
           Add Product
         </Button>
-      </div>
-      <div className="flex-1 overflow-auto rounded-md border">
-        <Table className="whitespace-nowrap">
-          <TableHeader className="bg-accent">
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <TableHead
-                    key={header.id}
-                    className="h-10"
-                    style={{ width: header.getSize() }}
-                  >
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                ))}
-              </TableRow>
-            ))}
-          </TableHeader>
-
-          <TableBody>
-            {table.getRowModel().rows.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                  className="hover:cursor-pointer h-12"
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results found.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-
-      <div className="flex items-center justify-between px-4">
-        <div className="text-muted-foreground hidden flex-1 text-sm lg:flex">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
-        <div className="flex w-full items-center gap-8 lg:w-fit">
-          <div className="hidden items-center gap-2 lg:flex">
-            <Label htmlFor="rows-per-page" className="text-sm font-medium">
-              Rows per page
-            </Label>
-            <Select
-              value={`${table.getState().pagination.pageSize}`}
-              onValueChange={(value) => {
-                table.setPageSize(Number(value));
-              }}
-            >
-              <SelectTrigger size="sm" className="w-20" id="rows-per-page">
-                <SelectValue
-                  placeholder={table.getState().pagination.pageSize}
-                />
-              </SelectTrigger>
-              <SelectContent side="top">
-                {[10, 20, 30, 40, 50].map((pageSize) => (
-                  <SelectItem key={pageSize} value={`${pageSize}`}>
-                    {pageSize}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="flex w-fit items-center justify-center text-sm font-medium">
-            Page {table.getState().pagination.pageIndex + 1} of{" "}
-            {table.getPageCount()}
-          </div>
-          <div className="ml-auto flex items-center gap-2 lg:ml-0">
-            <Button
-              variant="outline"
-              className="hidden h-8 w-8 p-0 lg:flex"
-              onClick={() => table.setPageIndex(0)}
-              disabled={!table.getCanPreviousPage()}
-            >
-              <span className="sr-only">Go to first page</span>
-              <ChevronsLeft />
-            </Button>
-            <Button
-              variant="outline"
-              className="size-8"
-              size="icon"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              <span className="sr-only">Go to previous page</span>
-              <ChevronLeft />
-            </Button>
-            <Button
-              variant="outline"
-              className="size-8"
-              size="icon"
-              onClick={() => table.nextPage()}
-              disabled={!table.getCanNextPage()}
-            >
-              <span className="sr-only">Go to next page</span>
-              <ChevronRight />
-            </Button>
-            <Button
-              variant="outline"
-              className="hidden size-8 lg:flex"
-              size="icon"
-              onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-              disabled={!table.getCanNextPage()}
-            >
-              <span className="sr-only">Go to last page</span>
-              <ChevronsRight />
-            </Button>
-          </div>
-        </div>
-      </div>
-    </div>
+      }
+      emptyMessage="No products found."
+    />
   );
 };
 
