dab3f10ccb4d0f7ff7501c4aa2caece927bb4daa
Nerwin Alamas <nerwinalamas@gmail.com>
2025-10-05 12:14:53 +0800

    refactor: update inventory page

--- app/(admin)/inventory/page.tsx
+++ app/(admin)/inventory/page.tsx
@@ -1,5 +1,16 @@
+import PageHeader from "@/components/page-header";
+import InventoryTable from "./_components/inventory-table";
+
 const InventoryPage = () => {
-  return <div>Inventory Page</div>;
+  return (
+    <div className="space-y-4">
+      <PageHeader
+        title="Inventory"
+        description="Track and manage your stock levels"
+      />
+      <InventoryTable />
+    </div>
+  );
 };
 
 export default InventoryPage;
