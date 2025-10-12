5b1b8004fa436fdb1ad66fd57a9a6fb5e118b400
Nerwin Alamas <nerwinalamas@gmail.com>
2025-10-12 13:38:19 +0800

    feat: add shadcn ui component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased bg-gray-100`}>{children}</body>
+      <body className={`${poppins.className} antialiased bg-gray-100`}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
