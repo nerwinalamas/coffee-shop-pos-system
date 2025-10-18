057f0f4f68e0fb31fe07fc20bdbe42ca8d25f494
Nerwin Alamas <nerwinalamas@gmail.com>
2025-10-18 12:34:47 +0800

    refactor: integrate app header

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,23 +1,13 @@
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+import AppHeader from "@/components/app-header";
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator
-            orientation="vertical"
-            className="mr-2 data-[orientation=vertical]:h-4"
-          />
-        </header>
+        <AppHeader />
         <main className="flex-1 p-4">{children}</main>
       </SidebarInset>
     </SidebarProvider>
