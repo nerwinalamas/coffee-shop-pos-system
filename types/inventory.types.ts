2378a72cd310169b7e8568377c94b0efe83fbd97
Nerwin Alamas <nerwinalamas@gmail.com>
2025-11-08 18:10:33 +0800

    refactor: update type to enum

--- types/inventory.types.ts
+++ types/inventory.types.ts
@@ -1,13 +1,19 @@
-export type StockStatus = "In Stock" | "Low Stock" | "Out of Stock";
+import { ProductCategory } from "./product.types";
 
 export type InventoryItem = {
   id: number;
   productName: string;
   sku: string;
-  category: "Coffee" | "Food" | "Dessert";
+  category: ProductCategory;
   quantity: number;
   reorderLevel: number;
   status: StockStatus;
   lastRestocked: string;
   image: string;
 };
+
+export enum StockStatus {
+  IN_STOCK = "In Stock",
+  LOW_STOCK = "Low Stock",
+  OUT_OF_STOCK = "Out of Stock",
+}
