306f2ae6e57c109b88eb81eadf431561faa71f1b
Nerwin Alamas <nerwinalamas@gmail.com>
2025-11-16 14:41:05 +0800

    feat: user role form

---
+++ components/forms/user-role-form.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { z } from "zod";
+import { UseFormReturn } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export const changeRoleSchema = z.object({
+  role: z.enum(["Admin", "Manager", "Staff"], {
+    message: "Role is required",
+  }),
+});
+
+export type ChangeRoleFormValues = z.infer<typeof changeRoleSchema>;
+
+interface UserRoleFormProps {
+  form: UseFormReturn<ChangeRoleFormValues>;
+  onSubmit: (values: ChangeRoleFormValues) => Promise<void> | void;
+  handleCancel: () => void;
+  submitLabel: string;
+  submitLoadingLabel: string;
+}
+
+const UserRoleForm = ({
+  form,
+  onSubmit,
+  handleCancel,
+  submitLabel,
+  submitLoadingLabel,
+}: UserRoleFormProps) => {
+  const isSubmitting = form.formState.isSubmitting;
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="role"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Role</FormLabel>
+              <Select
+                onValueChange={field.onChange}
+                defaultValue={field.value}
+                disabled={isSubmitting}
+              >
+                <FormControl>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select Role" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="Admin">Admin</SelectItem>
+                  <SelectItem value="Manager">Manager</SelectItem>
+                  <SelectItem value="Staff">Staff</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="w-full flex items-center gap-2 justify-end">
+          <Button
+            type="button"
+            variant="outline"
+            disabled={isSubmitting}
+            onClick={handleCancel}
+          >
+            Cancel
+          </Button>
+          <Button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? submitLoadingLabel : submitLabel}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default UserRoleForm;
