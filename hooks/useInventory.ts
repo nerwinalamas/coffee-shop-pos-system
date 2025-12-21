dbd2aa787ffd7e64ec6315cc336ecd3a3d9345a1
Nerwin Alamas <nerwinalamas@gmail.com>
2025-12-21 16:24:23 +0800

    refactor: update inventory hook

--- hooks/useInventory.ts
+++ hooks/useInventory.ts
@@ -1,12 +1,29 @@
 import { supabase } from "@/lib/supabase";
-import { Inventory } from "@/types/inventory.types";
+import { InventoryWithProduct } from "@/types/inventory.types";
 import { useQuery } from "@tanstack/react-query";
 
 export const useInventory = () => {
-  return useQuery<Inventory[]>({
+  return useQuery<InventoryWithProduct[]>({
     queryKey: ["inventory"],
     queryFn: async () => {
-      const { data, error } = await supabase.from("inventory").select("*");
+      const { data, error } = await supabase
+        .from("inventory")
+        .select(
+          `
+          *,
+          products (
+            id,
+            name,
+            price,
+            image,
+            category,
+            created_at,
+            updated_at
+          )
+        `
+        )
+        .order("created_at", { ascending: false });
+
       if (error) throw error;
       return data;
     },
