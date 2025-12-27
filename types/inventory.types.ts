aab24a2feb7582460fe7ff2b1d1ce5c4dbd1da6c
Nerwin Alamas <nerwinalamas@gmail.com>
2025-12-27 18:40:45 +0800

    chore: remove unused types

--- types/inventory.types.ts
+++ types/inventory.types.ts
@@ -1,23 +1,5 @@
-import { ProductCategory, Products } from "./product.types";
+import { Products } from "./product.types";
 import { Database } from "./supabase";
-
-export type InventoryItem = {
-  id: number;
-  productName: string;
-  sku: string;
-  category: ProductCategory;
-  quantity: number;
-  reorderLevel: number;
-  status: StockStatus;
-  lastRestocked: string;
-  image: string;
-};
-
-export enum StockStatus {
-  IN_STOCK = "In Stock",
-  LOW_STOCK = "Low Stock",
-  OUT_OF_STOCK = "Out of Stock",
-}
 
 export type Inventory = Database["public"]["Tables"]["inventory"]["Row"];
 
