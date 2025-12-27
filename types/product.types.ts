086480d52ccf8be5e392c2daeb84518ab0be2da1
Nerwin Alamas <nerwinalamas@gmail.com>
2025-12-27 18:40:54 +0800

    chore: remove unused product types

--- types/product.types.ts
+++ types/product.types.ts
@@ -1,13 +1,5 @@
 import { Database } from "./supabase";
 
-export type Product = {
-  id: number;
-  name: string;
-  price: number;
-  image: string;
-  category: ProductCategory;
-};
-
 export enum ProductCategory {
   COFFEE = "Coffee",
   FOOD = "Food",
