c2f05cfbf29d00617a882d96e641845c0c9f4b24
Nerwin Alamas <nerwinalamas@gmail.com>
2026-01-04 10:17:07 +0800

    feat: copy right

---
+++ components/copy-right.tsx
@@ -0,0 +1,11 @@
+import AppName from "./app-name";
+
+const CopyRight = () => {
+  return (
+    <p className="text-center text-xs text-gray-500 mt-6">
+      © 2026 <AppName />. All rights reserved.
+    </p>
+  );
+};
+
+export default CopyRight;
