f4188583424101f1cd7eae554f87a38df4aa7a81
Nerwin Alamas <nerwinalamas@gmail.com>
2026-01-25 09:33:46 +0800

    feat: get initials helper function

--- lib/utils.ts
+++ lib/utils.ts
@@ -45,3 +45,11 @@ export const getPageName = (pathname: string) => {
 
   return pageNames[lastSegment] || "Admin";
 };
+
+export const getInitials = (name: string) => {
+  const parts = name.split(" ");
+  if (parts.length >= 2) {
+    return `${parts[0][0]}${parts[1][0]}`.toUpperCase();
+  }
+  return name.substring(0, 2).toUpperCase();
+};
