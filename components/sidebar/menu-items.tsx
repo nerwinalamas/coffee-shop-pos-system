453c33f128905c46c12ce8fbcd1c916b92dbbaaa
Nerwin Alamas <nerwinalamas@gmail.com>
2026-01-31 13:40:46 +0800

    refactor: add link

--- components/sidebar/menu-items.tsx
+++ components/sidebar/menu-items.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { User2 } from "lucide-react";
 
@@ -5,18 +6,22 @@ const menuItems = [
   {
     icon: User2,
     label: "Profile Settings",
+    href: "/profile",
   },
   // {
   //   icon: CreditCard,
   //   label: "Billing",
+  //   href: "/billing",
   // },
   // {
   //   icon: Bell,
   //   label: "Notifications",
+  //   href: "/notifications",
   // },
   // {
   //   icon: Settings,
   //   label: "Settings",
+  //   href: "/settings",
   // },
 ];
 
@@ -26,9 +31,11 @@ const MenuItems = () => {
       {menuItems.map((item, index) => {
         const Icon = item.icon;
         return (
-          <DropdownMenuItem key={index}>
-            <Icon className="mr-2 h-4 w-4" />
-            <span>{item.label}</span>
+          <DropdownMenuItem key={index} asChild>
+            <Link href={item.href} className="cursor-pointer">
+              <Icon className="mr-2 h-4 w-4" />
+              <span>{item.label}</span>
+            </Link>
           </DropdownMenuItem>
         );
       })}
