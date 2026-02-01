d15589e98dc2afd1c78aa6fa9e2d4839768b6f69
Nerwin Alamas <nerwinalamas@gmail.com>
2026-02-01 10:28:02 +0800

    feat: page header

---
+++ app/(admin)/profile/_components/page-header.tsx
@@ -0,0 +1,12 @@
+const PageHeader = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold tracking-tight">Profile Settings</h1>
+      <p className="text-muted-foreground">
+        Manage your account information and security settings
+      </p>
+    </div>
+  );
+};
+
+export default PageHeader;
