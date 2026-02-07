eb20073d4827562f7d7a0273b7b3a60eb4b52b2a
Nerwin Alamas <nerwinalamas@gmail.com>
2026-02-07 12:23:30 +0800

    feat: transactions page

---
+++ app/(admin)/transactions/page.tsx
@@ -0,0 +1,7 @@
+import TransactionHistoryTable from "./_components/transaction-history-table";
+
+const TransactionsPage = () => {
+  return <TransactionHistoryTable />;
+};
+
+export default TransactionsPage;
