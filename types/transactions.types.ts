a1b7df42d21b4b8ec8a7cd97c6430b6a63dd1e09
Nerwin Alamas <nerwinalamas@gmail.com>
2026-02-08 10:32:58 +0800

    feat: transaction with items types

--- types/transactions.types.ts
+++ types/transactions.types.ts
@@ -1,3 +1,8 @@
 import { Database } from "./supabase";
+import { TransactionItems } from "./transaction-items.types";
 
 export type Transactions = Database["public"]["Tables"]["transactions"]["Row"];
+
+export type TransactionWithItems = Transactions & {
+  transaction_items: TransactionItems[];
+};
