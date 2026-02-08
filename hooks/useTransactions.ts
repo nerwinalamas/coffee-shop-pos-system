e637668180485717e523c748506928ee09b2b5ad
Nerwin Alamas <nerwinalamas@gmail.com>
2026-02-08 10:32:40 +0800

    refactor: join the transaction items to use transactions hook

--- hooks/useTransactions.ts
+++ hooks/useTransactions.ts
@@ -1,14 +1,23 @@
 import { createClient } from "@/lib/supabase/client";
-import { Transactions } from "@/types/transactions.types";
+import { TransactionWithItems } from "@/types/transactions.types";
 import { useQuery } from "@tanstack/react-query";
 
 export const useTransactions = () => {
   const supabase = createClient();
 
-  return useQuery<Transactions[]>({
+  return useQuery<TransactionWithItems[]>({
     queryKey: ["transactions"],
     queryFn: async () => {
-      const { data, error } = await supabase.from("transactions").select("*");
+      const { data, error } = await supabase
+        .from("transactions")
+        .select(
+          `
+          *,
+          transaction_items (*)
+        `,
+        )
+        .order("created_at", { ascending: false });
+
       if (error) throw error;
       return data;
     },
