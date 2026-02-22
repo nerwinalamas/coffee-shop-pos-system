dc5fa9386260bfd316a13730e19a48091181c73b
Nerwin Alamas <nerwinalamas@gmail.com>
2026-02-22 09:33:18 +0800

    chore: remove unused code

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function proxy(request: NextRequest) {
+export function proxy() {
   return NextResponse.next();
 }
 
