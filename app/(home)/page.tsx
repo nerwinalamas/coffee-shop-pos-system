8567e9b9c7fc7bdd8e70fce42e4e370c6e346104
Nerwin Alamas <nerwinalamas@gmail.com>
2026-02-28 09:27:11 +0800

    refactor: initial pos login page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,10 +2,19 @@ import Menu from "./_components/menu";
 import OrderDetails from "./_components/order-details";
 
 const Home = () => {
+  const isLoggedIn = true;
+
   return (
     <div className="container mx-auto grid grid-cols-12 gap-4 h-screen p-4">
-      <Menu />
-      <OrderDetails />
+      {isLoggedIn ? (
+        <>
+          <Menu />
+          <OrderDetails />
+        </>
+      ) : (
+        // POS login page
+        <div className="col-span-full bg-white rounded-lg shadow-sm h-full p-4 space-y-4 max-h-screen"></div>
+      )}
     </div>
   );
 };
