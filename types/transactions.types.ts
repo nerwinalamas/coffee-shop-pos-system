6c48d4d4d2a85bb6e731bfab53666f1654854bd1
Nerwin Alamas <nerwinalamas@gmail.com>
2026-03-01 08:32:11 +0800

    refactor: add cashier types

--- types/transactions.types.ts
+++ types/transactions.types.ts
@@ -1,8 +1,12 @@
 import { Database } from "./supabase";
 import { TransactionItems } from "./transaction-items.types";
+import { Profiles } from "./profiles.types";
 
 export type Transactions = Database["public"]["Tables"]["transactions"]["Row"];
 
+type TransactionCashier = Pick<Profiles, "first_name" | "last_name" | "role">;
+
 export type TransactionWithItems = Transactions & {
   transaction_items: TransactionItems[];
+  cashier: TransactionCashier | null;
 };
