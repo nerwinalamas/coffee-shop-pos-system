4cc1af5e57b998c4a489f194a974b02b9044f7ed
Nerwin Alamas <nerwinalamas@gmail.com>
2026-03-14 07:39:47 +0800

    feat: activity logs table

---
+++ components/activity-logs-table.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { ActivityLogs } from "@/types/activity-logs.types";
+import { formatDistanceToNow } from "date-fns";
+import { Badge } from "@/components/ui/badge";
+import { DataTable } from "@/components/data-table";
+import { useActivityLogs } from "@/hooks/useActivityLogs";
+import { getActionColor } from "@/lib/utils";
+
+const ActivityLogsTable = () => {
+  const { data, isLoading, error } = useActivityLogs();
+
+  const columns: ColumnDef<ActivityLogs>[] = [
+    {
+      accessorKey: "action",
+      header: "Action",
+      cell: ({ row }) => (
+        <Badge className={getActionColor(row.original.action)}>
+          {row.original.action}
+        </Badge>
+      ),
+    },
+    {
+      accessorKey: "subject",
+      header: "Subject",
+      cell: ({ row }) => (
+        <span className="capitalize text-sm">{row.original.subject}</span>
+      ),
+    },
+    {
+      accessorKey: "created_at",
+      header: "Time",
+      cell: ({ row }) => (
+        <span className="text-sm text-gray-500">
+          {row.original.created_at
+            ? formatDistanceToNow(new Date(row.original.created_at), {
+                addSuffix: true,
+              })
+            : "Unknown"}
+        </span>
+      ),
+    },
+  ];
+
+  return (
+    <DataTable
+      columns={columns}
+      data={data ?? []}
+      emptyMessage="No activity logs found."
+      searchPlaceholder="Search by user or subject"
+      isLoading={isLoading}
+      loadingText="Loading activity logs..."
+      error={error}
+      errorText="Error loading activity logs"
+    />
+  );
+};
+
+export default ActivityLogsTable;
