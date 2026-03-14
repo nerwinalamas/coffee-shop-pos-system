07f2db924ee7f0b8d95a8782f7f55611b37d6f71
Nerwin Alamas <nerwinalamas@gmail.com>
2026-03-14 08:12:49 +0800

    refactor: get activity logs with profile

--- hooks/useActivityLogs.ts
+++ hooks/useActivityLogs.ts
@@ -1,14 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import { createClient } from "@/lib/supabase/client";
-import { ActivityLogs } from "@/types/activity-logs.types";
+import { ActivityLogsWithProfile } from "@/types/activity-logs.types";
 
 export const useActivityLogs = () => {
   const supabase = createClient();
 
-  return useQuery<ActivityLogs[]>({
+  return useQuery<ActivityLogsWithProfile[]>({
     queryKey: ["activity-logs"],
     queryFn: async () => {
-      const { data, error } = await supabase.from("activity_logs").select("*");
+      const { data, error } = await supabase
+        .from("activity_logs")
+        .select(
+          `
+          *,
+          profiles (
+            first_name,
+            last_name,
+            email
+          )
+        `,
+        )
+        .not("user_id", "is", null)
+        .order("created_at", { ascending: false });
+
       if (error) throw error;
       return data;
     },
