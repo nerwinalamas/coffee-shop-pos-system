5560b471c3a73fa9bbc277f01da9cb287dbd8896
Nerwin Alamas <nerwinalamas@gmail.com>
2026-03-22 10:50:47 +0800

    chore: update seed for product image by using unsplash images

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "picsum.photos",
+        hostname: "images.unsplash.com",
         port: "",
         pathname: "/**",
       },
